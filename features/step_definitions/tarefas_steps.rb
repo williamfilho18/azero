e47f08013b3b14785c72ee0fe228798dfff883f0
Aluno <Aluno@Digital.net>
2018-07-24 22:47:03 -0300

    remover sprint3

--- features/step_definitions/tarefas_steps.rb
+++ features/step_definitions/tarefas_steps.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 Dado('que eu tenho uma tarefa com os seguintes atributos:') do |table|
   @tarefa = table.rows_hash
   DAO.new.deleta_tarefa(@tarefa[:nome])
@@ -38,35 +40,35 @@ end
 
 
 # Remover
+# require 'faker' no top deste arquivo
 
 Dado("que eu tenho uma tarefa indesejada") do
   @tarefa = {
-    :nome => 'Tarefa muito boa para morrer',
+    :nome => 'Tarefa ' + Faker::Lorem.characters(10),
     :data => '30/07/2018'
   }
+  @tags = []
   DAO.new.deleta_tarefa(@tarefa[:nome])
   steps %(
     Quando faço o cadastro dessa tarefa
   )
-
-  # @tarefas_page.wait_for_botao_novo
-  # @tarefas_page.botao_novo.click
-  # @tarefas_page.wait_for_adicionar
-  # @tarefas_page.adicionar.nova(@tarefa)
 end
 
 Quando("eu solicito a exclusão desta tarefa") do
   tarefa_encontrada = find('table tbody tr', text: @tarefa[:nome])
   tarefa_encontrada.find('#delete-button').click
-  sleep 10
 end
 
 Quando("confirmo esta solicitação") do
-  pending # Write code here that turns the phrase above into concrete actions
+  within('.modal-footer') do
+    click_button 'Sim'
+  end
 end
 
 Então("esta tarefa não deve ser exibida na lista") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @tarefas_page.busca(@tarefa[:nome])
+  painel = find('#task-board')
+  expect(painel).to have_content 'Hmm... nenhuma tarefa encontrada :('
 end
 
 Quando("desisto da confirmação") do
