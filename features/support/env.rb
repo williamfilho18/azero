37424339b4aebc7fa066017069e40979aea2060d
williamfilho18 <41345561+williamfilho18@users.noreply.github.com>
2018-07-25 22:12:57 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,18 @@
 require 'capybara'
 require 'capybara/cucumber'
 require 'site_prism'
+require 'poltergeist'
 
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+  Capybara.register_driver :selenium do |app|
+    Capybara::Poltergeist::Driver.new(app, { js_errors: false })
+  end
+end
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = :selenium
   config.app_host = 'https://mark7.herokuapp.com'
 end
 
